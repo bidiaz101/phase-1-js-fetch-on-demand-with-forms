d3de388063a32f4cdcb5c717e03ebd1224175a59
Brian Diaz <bidiaz101@gmail.com>
2021-08-20 13:28:34 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', e => {
+      e.preventDefault();
+      const inputValue = e.target.searchByID.value;
+      console.log(inputValue)
+
+      fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then(resp => resp.json())
+        .then(json => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = json.title;
+            summary.innerText = json.summary;
+        });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
